337158890ccba3d660b431ef74db23ff748028a5
1942130635 <1942130635@qq.com>
2021-06-26 17:26:51 +0800

    stim300

---
+++ cmake-build-debug/stim300/CMakeFiles/stim300_driver_lib.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hx/stim300/src/stim300/src/datagram_parser.cpp" "/home/hx/stim300/src/cmake-build-debug/stim300/CMakeFiles/stim300_driver_lib.dir/src/datagram_parser.cpp.o"
+  "/home/hx/stim300/src/stim300/src/driver_stim300.cpp" "/home/hx/stim300/src/cmake-build-debug/stim300/CMakeFiles/stim300_driver_lib.dir/src/driver_stim300.cpp.o"
+  "/home/hx/stim300/src/stim300/src/serial_unix.cpp" "/home/hx/stim300/src/cmake-build-debug/stim300/CMakeFiles/stim300_driver_lib.dir/src/serial_unix.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"driver_stim300\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../stim300/include"
+  "../stim300/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
